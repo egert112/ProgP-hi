49d7f54e855d6924776fe28f9d06f1dc43c61bf0
if17 <if17@A406-21.INFORMAATIKA>
2018-02-01 10:23:14 +0200

    Uuendused

--- algus/src/algus/Algus.java
+++ algus/src/algus/Algus.java
@@ -14,7 +14,21 @@ public class Algus {
     /**Programmi töö alguskoht
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+     public static void ilmakommentaar(int temperatuur) {
+        if(temperatuur>0){System.out.println("sulab");}
+        else{System.out.println("jäätab");}
+     }
+     public static void Toasoojus(int toasoojus) {
+        if(toasoojus<0){System.out.println("Väga külm");}
+        else if(toasoojus>=19){System.out.println("Soe");}
+        else if(toasoojus>50){System.out.println("Sellist soojust ei eksisteeri");}
+        else {System.out.println("Kõrvetavalt palav");}
+     }
+public static void main(String[] args) {
         System.out.println("Hello, Web!");
+        ilmakommentaar(3);
+        Toasoojus(21);
+        Toasoojus(-2);
+        Toasoojus(100);
        }
 }
